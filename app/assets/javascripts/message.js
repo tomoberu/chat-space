3be2ff8c8dd8ca42a90bc798cf7a08caf914c7aa
tomoberu <57937124+tomoberu@users.noreply.github.com>
2019-12-02 16:20:03 +0900

    Ajax-brunch

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,15 +4,7 @@ $(function(){
         let image_html = "";
         if (message.image) {
            image_html = `<img src=${message.image} ></img>`
-          //image変数に<img src=${message.image} >を代入するコードを記入する
         }
-        // }else
-        // {
-        //   //let image = ""
-        //   //image変数に空欄を代入する
-        // }
-        
-        //if (message.image) {
           var html = 
           `<div class="contents__top" data-message-id=${message.id}>
             <div class="contents__top__name">
@@ -26,31 +18,11 @@ $(function(){
               <p class="lower-message__content">
                 ${message.content}
               </p>
-            </div>
               ${image_html}
           </div>`
           return html;
-       // } else {
-          var html =
-          `<div class="contents__top" data-message-id=${message.id}></div>
-            <div class="contents__top__name">
-                  ${message.user_name}
-            </div>
-            <div class="contents__top__date">
-                  ${message.date}
-            </div>
-          </div>
-          <div class="contents__top__message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-             </div>
-          </div>`
-          return html;
-       // };
       }
   $('#new_message').on('submit', function(e){
-    console.log("ok")
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
